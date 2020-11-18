bd33610d1fd6ec9a891cce65e5a8fab0fd65f713
stefanmm <7655575+stefanmm@users.noreply.github.com>
2020-11-18 22:48:14 +0100

    Added links in the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,10 @@
     <footer class="footer has-text-white  has-background-black ">
         <div class="content has-text-centered">
             <p>
-            By <a href="https://www.stefanmarjanov.com/" target="_blank" class="has-text-info	">Stefan Marjanov</a>
+            By <a href="https://www.stefanmarjanov.com/" target="_blank" class="has-text-info	">Stefan Marjanov</a>. Thanks to:
+            <a href="https://fontawesome.com/" target="_blank" rel="nofollow" class="has-text-info">Fontawesome</a> | 
+            <a href="https://www.freepik.com/" target="_blank" rel="nofollow" class="has-text-info">Freepik</a> | 
+            <a href="https://www.flaticon.com/" target="_blank" rel="nofollow" class="has-text-info">Flaticon</a>
             </p>
         </div>
         </footer>
